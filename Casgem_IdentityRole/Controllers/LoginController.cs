6478f7796ee3c8997478c660a7a67ab81de4be83
MuratYucedag <yucedagmurat23@gmail.com>
2023-08-02 10:19:48 +0300

    ...

---
+++ Casgem_IdentityRole/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Casgem_IdentityRole.DAL;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem_IdentityRole.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index
+        {
+
+        }
+    }
+}
--- Casgem_IdentityRole/Program.cs
+++ Casgem_IdentityRole/Program.cs
@@ -1,6 +1,10 @@
+using Casgem_IdentityRole.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
